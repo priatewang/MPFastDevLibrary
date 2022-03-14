d4dd20fbb0161feb1ecc2c6893df93ebee01efe2
海盗 <62652363+priatewang@users.noreply.github.com>
2022-03-14 20:51:35 +0800

    创建服务管理项目

---
+++ MPServiceController/ServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPServiceController
+{
+    public class ServiceManager
+    {
+        public static List<NetServiceHost> Services { get; set; } = new List<NetServiceHost>();
+
+
+    }
+}
