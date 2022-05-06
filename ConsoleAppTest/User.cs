d3e71a477ca4aaf4fee8fa103cd4a169a7083da0
王强 <w498085656@outlook.com>
2022-05-06 20:59:16 +0800

    完成Ioc容器基础功能搭建（存入和取出）

---
+++ ConsoleAppTest/User.cs
@@ -0,0 +1,26 @@
+﻿/*----------------------------------------------------------------
+// 创建时间：2022/5/6 20:54:16
+// 开发者： WQ
+// 文件名： User
+// CLR版本：4.0.30319.42000
+// 命名空间：ConsoleAppTest
+// 功能描述：
+// 使用说明：
+----------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    public class User
+    {
+
+        public void Send()
+        {
+            Console.WriteLine(" User Send  ok....");
+        }
+    }
+}
