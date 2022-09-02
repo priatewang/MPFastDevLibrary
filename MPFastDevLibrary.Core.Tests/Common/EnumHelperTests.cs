decf8319af22015e7355589b041d47d2bd5aeb7d
pwang <62652363+priatewang@users.noreply.github.com>
2022-09-02 10:26:40 +0800

    添加帮助类的单元测试项目，测试枚举和映射辅助类

---
+++ MPFastDevLibrary.Core.Tests/Common/EnumHelperTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPFastDevLibrary.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+
+namespace MPFastDevLibrary.Common.Tests
+{
+    [TestClass()]
+    public class EnumHelperTests
+    {
+        enum MyEnum
+        {
+            [Description("测试1")]
+            Test1,
+            [Description("测试2")]
+            Test2,
+            [Description("测试3")]
+            Test3,
+        }
+
+        [TestMethod()]
+        public void GetEnumContentsTest()
+        {
+            var list = EnumHelper.GetEnumContents<MyEnum>();
+            Assert.IsNotNull(list);
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual("Test1", list[0]);
+        }
+
+        [TestMethod()]
+        public void GetEnumDescriptionsTest()
+        {
+            var list=EnumHelper.GetEnumDescriptions<MyEnum>();
+            Assert.IsNotNull(list);
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual("测试1", list[0]);
+        }
+
+        [TestMethod()]
+        public void GetDescriptionTest()
+        {
+           var str= MyEnum.Test1.GetDescription();
+            Assert.AreEqual("测试1", str);
+        }
+
+        [TestMethod()]
+        public void EnumConvertByDescriptionTest()
+        {
+            var em = EnumHelper.EnumConvertByDescription<MyEnum>("测试2");
+            Assert.AreEqual(MyEnum.Test2,em);
+        }
+    }
+}
---
+++ MPFastDevLibrary.Core.Tests/Common/MapHelperTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPFastDevLibrary.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Common.Tests
+{
+    [TestClass()]
+    public class MapHelperTests
+    {
+        class TestClass1
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Desc { get; set; }
+
+
+
+        }
+
+        class TestClass2
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Desc { get; set; }
+
+
+
+        }
+
+
+
+        [TestMethod()]
+        public void MappingTest()
+        {
+            TestClass1 t1 = new TestClass1()
+            {
+                Id = 1,
+                Desc = "1号",
+                Name = "Name1",
+            };
+            var t2 = MapHelper.Mapping<TestClass2, TestClass1>(t1);
+
+            Assert.AreEqual(1, t2.Id);
+            Assert.AreEqual("1号",t2.Desc);
+            Assert.AreEqual("Name1",t2.Name);
+
+        }
+
+        [TestMethod()]
+        public void MappingTest1()
+        {
+            TestClass1 t1 = new TestClass1()
+            {
+                Id = 1,
+                Desc = "1号",
+                Name = "Name1",
+            };
+
+            TestClass1 t2=new TestClass1();
+            MapHelper.Mapping(ref t2, t1);
+
+            Assert.AreEqual(1, t2.Id);
+            Assert.AreEqual("1号", t2.Desc);
+            Assert.AreEqual("Name1", t2.Name);
+        }
+    }
+}
