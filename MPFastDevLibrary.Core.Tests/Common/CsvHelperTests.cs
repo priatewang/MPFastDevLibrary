08b34ad4d06d35e795d1e3691f265b9e8c8f6df5
pwang <62652363+priatewang@users.noreply.github.com>
2022-09-04 12:55:15 +0800

    增加CSV读写datatable和测试

---
+++ MPFastDevLibrary.Core.Tests/Common/CsvHelperTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPFastDevLibrary.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Common.Tests
+{
+    [TestClass()]
+    public class CsvHelperTests
+    {
+        [TestMethod()]
+        public void WriteToCsvByDataTableTest()
+        {
+            DataTable dt = new DataTable();
+            for (int i = 0; i < 200; i++)
+            {
+                dt.Columns.Add();
+            }
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                string[] arr = new string[200];
+                for (int j = 0; j < 200; j++)
+                {
+                    arr[j] = random.Next(100).ToString();
+                }
+                dt.Rows.Add(arr);
+            }
+
+            bool issuccess = CsvHelper.WriteToCsvByDataTable(dt, "csvTest.csv", false);
+            Assert.IsTrue(issuccess);
+            var lines = File.ReadAllLines("csvTest.csv");
+            Assert.AreEqual(100, lines.Length);
+        }
+
+
+        [TestMethod()]
+        public void ReadCsvToDataTableTest()
+        {
+            var dt=  CsvHelper.ReadCsvToDataTable("csvTest.csv", false); 
+            Assert.IsNotNull(dt);
+            Assert.AreEqual(200, dt.Columns.Count);
+            Assert.AreEqual(100, dt.Rows.Count);
+        }
+
+        [TestMethod()]
+        public void ReadCsvByStreamTest()
+        {
+            var dt = CsvHelper.ReadCsvByStream("csvTest.csv", false);
+            Assert.IsNotNull(dt);
+            Assert.AreEqual(200, dt.Columns.Count);
+            Assert.AreEqual(100, dt.Rows.Count);
+        }
+
+      
+
+        [TestMethod()]
+        public void ReadCsvTest()
+        {
+            var lines = CsvHelper.ReadCsv("csvTest.csv",false);
+            Assert.AreEqual(100, lines.Count);
+
+        }
+    }
+}
--- MPFastDevLibrary.Core/Common/CsvHelper.cs
+++ MPFastDevLibrary.Core/Common/CsvHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -12,31 +13,38 @@ namespace MPFastDevLibrary.Common
     public class CsvHelper
     {
         /// <summary>
-        /// 
+        /// 读取Csv文件，加载到DataTable
         /// </summary>
-        /// <param name="path"></param>
-        /// <param name="hasTitle"></param>
-        /// <param name="SafeLevel">0:错误格式行正常添加；1：错误行忽略（不添加），2：出现错误弹出异常</param>
+        /// <param name="path">csv文件路径</param>
+        /// <param name="hasTitle">是否有标题行</param>
+        /// <param name="SafeLevel">安全等级：0:错误格式行正常添加；1：错误行忽略（不添加），2：出现错误弹出异常</param>
         /// <returns></returns>
         public static DataTable ReadCsvToDataTable(string path, bool hasTitle = false, int SafeLevel = 0)
         {
 
             DataTable dt = new DataTable();
             var lines = ReadCsv(path, false);
-            if (hasTitle)
-            {
-                string title = lines[0];
-                string[] values = title.Split(',');
-                for (int i = 0; i < values.Length; i++)
-                {
-                    dt.Columns.Add();
-                }
-                lines.RemoveAt(0);
-            }
+            bool isFirst = true;
 
             foreach (var item in lines)
             {
                 string[] values = item.Split(',');
+                if (isFirst)
+                {
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        dt.Columns.Add();
+                    }
+                    if (hasTitle)
+                    {
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            dt.Columns[i].ColumnName = values[i];
+                        }
+                        continue;
+                    }
+                    isFirst = false;
+                }
                 if (values.Length == dt.Columns.Count)
                 {
                     dt.Rows.Add(values);
@@ -70,7 +78,131 @@ namespace MPFastDevLibrary.Common
             return dt;
         }
 
+        /// <summary>
+        /// 以文件流形式读取Csv文件，加载到DataTable
+        /// </summary>
+        /// <param name="path">csv文件路径</param>
+        /// <param name="hasTitle">是否有标题行</param>
+        /// <param name="SafeLevel">安全等级：0:错误格式行正常添加；1：错误行忽略（不添加），2：出现错误弹出异常</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DataTable ReadCsvByStream(string path, bool hasTitle = false, int SafeLevel = 0)
+        {
+            DataTable dt = new DataTable();
+            bool isFirst = true;
 
+            using (StreamReader sr = new StreamReader(path))
+            {
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    string[] values = line.Split(',');
+                    if (isFirst)
+                    {
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            dt.Columns.Add();
+                        }
+                        isFirst = false;
+                    }
+
+                    //有表头则添加
+                    if (hasTitle)
+                    {
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            dt.Columns[i].ColumnName = values[i];
+                        }
+                        hasTitle = false;
+                    }
+                    else
+                    {
+                        if (values.Length == dt.Columns.Count)
+                        {
+                            dt.Rows.Add(values);
+                        }
+                        else
+                        {
+                            switch (SafeLevel)
+                            {
+                                case 0:
+                                    if (values.Length > dt.Columns.Count)
+                                    {
+                                        var res = values.ToList();
+                                        res.RemoveRange(dt.Columns.Count, values.Length - dt.Columns.Count);
+                                        dt.Rows.Add(res.ToArray());
+                                    }
+                                    else
+                                    {
+                                        dt.Rows.Add(values);
+                                    }
+                                    break;
+                                case 1:
+                                    continue;
+                                default:
+                                    throw new Exception("CSV格式错误：表格各行列数不一致");
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// 以文件流形式写入DataTable
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="path"></param>
+        /// <param name="hasTitle"></param>
+        /// <returns></returns>
+        public static bool WriteToCsvByDataTable(DataTable dt, string path, bool hasTitle = false)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                //输出标题行（如果有）
+                if (hasTitle)
+                {
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        sw.Write(dt.Columns[i].ColumnName);
+                        if (i != dt.Columns.Count - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+                    sw.WriteLine();
+                }
+
+                //输出文件内容
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        sw.Write(dt.Rows[i][j].ToString());
+                        if (j != dt.Columns.Count - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+                    sw.WriteLine();
+                }
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// 读取Csv，返回行集合
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="hasTitle"></param>
+        /// <returns></returns>
         public static List<string> ReadCsv(string path, bool hasTitle)
         {
             if (!File.Exists(path))
