0995fde17b0d71be579a49406d23b6cc9e1fbad8
pwang <wqiang1956@126.com>
2022-11-19 10:44:23 +0800

    ioc注册测试

---
+++ PakeageTest/IocTestInterface.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleAppTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PakeageTest
+{
+
+    public interface ITestService
+    {
+        void Send(string msg);
+
+    }
+
+    public class AService : ITestService
+    {
+        public void Send(string msg)
+        {
+            Console.WriteLine("AService :" + msg);
+        }
+    }
+
+
+    public class BService : ITestService
+    {
+        public void Send(string msg)
+        {
+            Console.WriteLine("BService :" + msg);
+        }
+    }
+
+
+    internal class IocTestInterface
+    {
+        ITestService _testService;
+
+        public IocTestInterface()
+        {
+            _testService = Program._testService;
+        }
+
+        public IocTestInterface(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+
+        public void UseService(string mesg)
+        {
+            Program._testService.Send(mesg);
+        }
+
+
+
+
+    }
+}
--- PakeageTest/Program.cs
+++ PakeageTest/Program.cs
@@ -3,6 +3,7 @@ using Autofac;
 using PakeageTest;
 using System;
 using System.IO.Compression;
+using System.Net.Http.Headers;
 
 namespace ConsoleAppTest
 {
@@ -13,23 +14,61 @@ namespace ConsoleAppTest
             Console.WriteLine("Hello World!");
 
             #region Autofac测试
-            var builder = new ContainerBuilder();
-            // builder.RegisterType<MyService>().As<IMyService>();
-            builder.RegisterType(typeof(User));
-            builder.RegisterType(typeof(MyService)).As(typeof(IMyService));
-            var container = builder.Build();
+            //var builder = new ContainerBuilder();
+            //// builder.RegisterType<MyService>().As<IMyService>();
+            //builder.RegisterType(typeof(User));
+            //builder.RegisterType(typeof(MyService)).As(typeof(IMyService));
+            //var container = builder.Build();
 
-            var ims = container.Resolve<IMyService>();
-            var user=container.Resolve<User>();
+            //var ims = container.Resolve<IMyService>();
+            //var user=container.Resolve<User>();
 
-            ims.Send("111");
+            //ims.Send("111");
 
-            user.Send();
+            //user.Send();
 
             #endregion
 
+            //#region Ioc两个实现类切换测试
+            //var builder = new ContainerBuilder();
+            //builder.RegisterType<AService>().As<ITestService>().SingleInstance();
+            //builder.RegisterType<IocTestInterface>();
+            //var container = builder.Build();
+
+            //var test = container.Resolve<IocTestInterface>();
+
+            //test.UseService("test 修改前");
+
+            //builder.RegisterType<BService>().As<ITestService>().SingleInstance();
+            //var test2 = container.Resolve<IocTestInterface>();
+            //test2.UseService("test2 修改后，不修改container");
+
+            //test.UseService("test 修改后,不修改类");
+
+            //var container2 = builder.Build();
+            //var test3 = container2.Resolve<IocTestInterface>();
+            //test3.UseService("test3 修改后，修改container");
+
+            //#endregion
+
+            #region 静态切换
+            IocTestInterface test = new IocTestInterface();
+            test.UseService("切换前");
+
+            _testService = new BService();
+
+            test.UseService("切换后");
+
+            #endregion
+
+
             Console.ReadKey();
         }
+
+        public static ITestService _testService = new AService();
+
+
+
         static void Write1(object o)
         {
             Console.WriteLine("write1:" + o.ToString());
