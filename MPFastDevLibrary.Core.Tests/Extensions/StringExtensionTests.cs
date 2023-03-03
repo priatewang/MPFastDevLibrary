a4d4722c57f63a084fdd98eaf9811f0ef57c1cdb
wangqiang <wqiang1956@126.com>
2023-03-03 17:31:46 +0800

    添加string的扩展方法，判断是否数字，整数，字母； 删除Ioc，迁移到项目PwDevIoc

---
+++ MPFastDevLibrary.Core.Tests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPFastDevLibrary.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Extensions.Tests
+{
+    [TestClass()]
+    public class StringExtensionTests
+    {
+        [TestMethod()]
+        public void IsIntegerTest()
+        {
+            string s = "12443";
+            var res1 = s.IsInteger();
+            Assert.IsTrue(res1);
+
+            string s2 = "sdw1231";
+            var res2 = s2.IsInteger();
+            Assert.IsFalse(res2);
+        }
+
+        [TestMethod()]
+        public void IsDoubleTest()
+        {
+            string s = "124.43";
+            var res1 = s.IsDouble();
+            Assert.IsTrue(res1);
+            string s2 = "123.34231";
+            var res2 = s2.IsDouble();
+            Assert.IsTrue(res2);
+            string s3 = "dsajdghah1231";
+            var res3 = s3.IsDouble();
+            Assert.IsFalse(res3);
+        }
+
+        [TestMethod()]
+        public void IsNumberTest()
+        {
+            string s = "124.43";
+            var res1 = s.IsNumber();
+            Assert.IsFalse(res1);
+            string s2 = "1231";
+            var res2 = s2.IsNumber();
+            Assert.IsTrue(res2);
+            string s3 = "dsajdghah1231";
+            var res3 = s3.IsNumber();
+            Assert.IsFalse(res3);
+        }
+
+        [TestMethod()]
+        public void IsCharactersTest()
+        {
+            string s = "adasasf";
+            var res1 = s.IsCharacters();
+            Assert.IsTrue(res1);
+            string s2 = "ADSscscvf";
+            var res2 = s2.IsCharacters();
+            Assert.IsTrue(res2);
+            string s3 = "dsajdghah1231";
+            var res3 = s3.IsCharacters();
+            Assert.IsFalse(res3);
+        }
+    }
+}
---
+++ MPFastDevLibrary.Core/Extensions/StringExtension.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Common
+{
+    /// <summary>
+    /// string 扩展方法类
+    /// </summary>
+    public static class StringExtension
+    {
+        const string numberRegex = "^[0-9]*$";
+
+        const string CharactersRegex = "^[A-Za-z]+$";
+
+
+        /// <summary>
+        /// 判断string是否是整数int
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsInteger(this string str)
+        {
+            if (str == null)
+                return false;
+            return int.TryParse(str, out var result);
+        }
+
+
+
+        /// <summary>
+        /// 是否浮点数
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsDouble(this string str)
+        {
+            if (str == null) return false;
+            return double.TryParse(str, out var result);
+        }
+
+        /// <summary>
+        /// 判断string字符串是否是纯数字
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNumber(this string str)
+        {
+            if (str == null) return false;
+            return Regex.IsMatch(str, numberRegex);
+        }
+
+        /// <summary>
+        /// 判断string字符串是否纯字母
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsCharacters(this string str)
+        {
+            if (str == null) return false;
+            return Regex.IsMatch(str, CharactersRegex);
+        }
+
+    }
+}
