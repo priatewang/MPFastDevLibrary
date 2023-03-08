6235288f7c3ef6216a9916c47246391750e29d01
wangqiang <wqiang1956@126.com>
2023-03-08 17:27:58 +0800

    List和ObservableCollection扩展方法

---
+++ MPFastDevLibrary.Core/Extensions/ListExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Extensions
+{
+    /// <summary>
+    /// List扩展0
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// 如果不存在，则添加
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="t"></param>
+        public static void AddWithout<T>(this List<T> list, T t)
+        {
+            if (!list.Contains(t))
+            {
+                list.Add(t); 
+            }
+        }
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="dels"></param>
+        public static void DeleteRange<T>(this List<T> list,IEnumerable<T> dels)
+        {
+            list.RemoveAll(t => dels.Contains(t));
+        }
+
+
+    }
+}
---
+++ MPFastDevLibrary.Core/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Common
+{
+    /// <summary>
+    /// ObservableCollection扩展
+    /// </summary>
+    public static class ObservableCollectionExtensions
+    {
+        private static void Test()
+        {
+            ObservableCollection<int> values = new ObservableCollection<int> { 1, 2, 3, 4, 5, 6, };
+            values.Add(1);
+            values.Remove(1);
+        }
+
+
+        /// <summary>
+        /// 批量添加ObservableCollection的项（循环添加）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obs"></param>
+        /// <param name="collection"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void AddRange<T>(this ObservableCollection<T> obs, IEnumerable<T> collection)
+        {
+            if (obs == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (collection == null)
+            {
+                return;
+            }
+            foreach (var item in collection)
+            {
+                obs.Add(item);
+            }
+        }
+
+        public static bool RemoveRange<T>(this ObservableCollection<T> obs,IEnumerable<T> collection)
+        {
+            if (obs == null)
+                throw new ArgumentNullException();
+            if (collection == null)
+                return false;
+            foreach (var item in collection)
+            {
+                obs.Remove(item);
+            }
+        }
+
+
+    }
+}
