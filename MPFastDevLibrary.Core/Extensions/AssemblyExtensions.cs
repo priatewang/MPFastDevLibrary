76d488042caa17382c95bb760e1674539d63eb50
wangqiang <wqiang1956@126.com>
2023-04-07 15:58:28 +0800

    增加Assembly方法扩展和Array2DToStringConverter

---
+++ MPFastDevLibrary.Core/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPFastDevLibrary.Extensions
+{
+    /// <summary>
+    /// Assembly方法扩展
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// 获取项目下面所有包含spaceName字段的程序集
+        /// </summary>
+        /// <param name="domain"></param>
+        /// <param name="spaceName"></param>
+        /// <returns></returns>
+        public static List<Assembly> GetReferanceAssemblies(this AppDomain domain, string spaceName)
+        {
+            var list = new List<Assembly>();
+            domain.GetAssemblies().ToList().ForEach(i =>
+            {
+                if (i.FullName.Contains(spaceName))
+                {
+                    list.Add(i);
+                }
+                GetReferanceAssemblies(i, list, spaceName);
+            });
+            return list;
+        }
+
+        static void GetReferanceAssemblies(Assembly assembly, List<Assembly> list, string spaceName)
+        {
+            assembly.GetReferencedAssemblies().ToList().ForEach(i =>
+            {
+                if (!i.Name.Contains(spaceName))
+                    return;
+                var ass = Assembly.Load(i);
+                if (!list.Contains(ass))
+                {
+                    list.Add(ass);
+                    GetReferanceAssemblies(ass, list, spaceName);
+                }
+            });
+        }
+
+    }
+}
---
+++ MPFastDevLibrary.Mvvm/Converter/Array2DToStringConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MPFastDevLibrary.Mvvm.Converter
+{
+    public class Array2DToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var array2d = value as double[,];
+            string res = "";
+            for (int i = 0; i < array2d.GetLength(0); i++)
+            {
+                string two = "";
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                {
+                    two += array2d[i, j].ToString() + ",";
+                }
+                two = two.Remove(two.Length - 1);
+                res += two + "|";
+            }
+            res = res.Remove(res.Length - 1);
+
+            return res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return default(double[,]);
+            var str = value.ToString();
+            var array = str.Split('|');
+            int cout = array[0].Split(',').Count();
+            double[,] doubles = new double[array.Count(), cout];
+            for (int i = 0; i < array.Length; i++)
+            {
+                var values = array[i].Split(',');
+                for (int j = 0; j < values.Length; j++)
+                {
+                    if (double.TryParse(values[j], out double d))
+                    {
+                        doubles[i, j] = d;
+
+                    }
+                }
+            }
+            return doubles;
+        }
+    }
+}
